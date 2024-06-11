5365812e5d57df5e381e891caffc465312ebf615
vishaldhakal <vishaldhakal96@gmail.com>
2024-06-11 18:49:51 +0545

    feat: Add lazy loading for images in schedule pages

--- src/app/(home)/schedule/page.js
+++ src/app/(home)/schedule/page.js
@@ -1,11 +1,11 @@
 import ScheduleCall from "@/components/ScheduleCall";
 import ScheduleTime from "@/components/ScheduleTime";
+import Script from "next/script";
 import React from "react";
 
 const page = () => {
   return (
     <div className="container">
-      {/* <ScheduleTime /> */}
       <ScheduleCall />
     </div>
   );
--- src/app/(home)/schedule/time/page.js
+++ src/app/(home)/schedule/time/page.js
@@ -1,12 +1,28 @@
-import ScheduleTime from '@/components/ScheduleTime'
-import React from 'react'
+import ScheduleTime from "@/components/ScheduleTime";
+import React from "react";
+import Script from "next/script";
 
 const page = () => {
   return (
-    <div className='container'>
+    <div className="container">
+      <div
+        class="calendly-inline-widget"
+        data-url="https://calendly.com/vishaldhakal96/30min"
+        style={{
+          minWidth: "320px",
+          height: "580px",
+          display: "block",
+          margin: "0 auto",
+        }}
+      ></div>
+      <Script
+        type="text/javascript"
+        src="https://assets.calendly.com/assets/external/widget.js"
+        async
+      ></Script>
       <ScheduleTime />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
