ff1f2a19b38f7a2ac6fa68a8ac8c49ff6ff7be52
iam8uman <me8848you@gmail.com>
2024-06-14 13:02:11 +0545

    calender event fetch success

--- src/app/(home)/schedule/time/page.js
+++ src/app/(home)/schedule/time/page.js
@@ -18,68 +18,63 @@
 // };
 
 // export default page;
-"use client"
-import React, { useEffect, useState } from 'react'
-import { gapi } from 'gapi-script'
-import Event from '@/components/CalendarEvent'
+"use client";
+import React, { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+import Event from "@/components/CalendarEvent";
 
 function Page() {
-    const [events, setEvents] = useState([])
+  const [events, setEvents] = useState([]);
 
-    const calendarID = process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID
-    const apiKey = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY
-    const accessToken = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN
+  const calendarID = process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID;
+  const apiKey = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY;
+  const accessToken = process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN;
 
+  console.log(calendarID, apiKey, accessToken)
 
-    const getEvents = (calendarID, apiKey,accessToken) => {
-        function initiate() {
-            gapi.client
-                .init({
-                    apiKey: apiKey,
-                })
-                .then(function () {
-                    return gapi.client.request({
-                        path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
-                        headers: {
-                          Authorization: `Bearer ${accessToken}`,
-                      },
-                    })
-                })
-                .then(
-                    (response) => {
-                        let events = response.result.items
-                        setEvents(events)
-                    },
-                    
-                    function (err) {
-                      console.log(err,"errorrooror")
-                        return [false, err]
-                    }
-                )
-                
-        }
-        gapi.load('client', initiate)
+  const getEvents = (calendarID, apiKey, accessToken) => {
+    function initiate() {
+      gapi.client
+        .init({
+          apiKey: apiKey,
+        })
+        .then(function () {
+          return gapi.client.request({
+            path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+          });
+        })
+        .then(
+          (response) => {
+            let events = response.result.items;
+            setEvents(events);
+          },
+          function (err) {
+            console.error("Error during request:", err);
+            return [false, err];
+          }
+        );
     }
+    gapi.load("client", initiate);
+  };
 
-    useEffect(() => {
-        const events = getEvents(calendarID, apiKey,accessToken)
-        setEvents(events)
-    }, [])
+  useEffect(() => {
+    getEvents(calendarID, apiKey, accessToken);
+  }, []);
 
-    return (
-        <div className="App flex flex-col justify-center py-8">
-            <h1 className="mb-4 text-2xl font-bold">
-                React App with Google Calendar API!
-                <ul>
-                    {events?.map((event) => (
-                        <li key={event.id} className="flex justify-center">
-                            <Event description={event.summary} />
-                        </li>
-                    ))}
-                </ul>
-            </h1>
-        </div>
-    )
+  return (
+    <div className="App flex flex-col justify-center py-8">
+      <h1 className="mb-4 text-2xl font-bold">
+        React App with Google Calendar API!
+        <ul>
+          {events?.map((event) => (
+            <li key={event.id} className="flex justify-center">
+              <Event description={event.summary} />
+            </li>
+          ))}
+        </ul>
+      </h1>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
