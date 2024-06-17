2f7a4bad16ebdcd00efe63dbeb7be4de529fb0cd
iam8uman <me8848you@gmail.com>
2024-06-17 15:18:22 +0545

    multiple event created success

--- src/constant/data.js
+++ src/constant/data.js
@@ -6,6 +6,9 @@ export const scheduleData = [
       subtitle: "An unparalleled investment experience.",
       duration: "60 Min",
       link: "/schedule/time?id=1",
+      calendarId:process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID_1,
+      accessToken: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN_1,
+      apiKey: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY_1,
     },
     {
       id: 2,
@@ -13,6 +16,10 @@ export const scheduleData = [
       subtitle: "An unparalleled investment experience.",
       duration: "60 Min",
       link: "/schedule/time?id=2",
+      calendarId:process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID_2,
+      accessToken: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN_2,
+      apiKey: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY_2
+
     },
     {
       id: 3,
@@ -20,6 +27,9 @@ export const scheduleData = [
       subtitle: "An unparalleled investment experience.",
       duration: "30 Min",
       link: "/schedule/time?id=3",
+      calendarId:process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID_3,
+      accessToken: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN_3,
+      apiKey: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY_3
     },
     {
       id: 4,
@@ -27,6 +37,9 @@ export const scheduleData = [
       subtitle: "An unparalleled investment experience.",
       duration: "30 Min",
       link: "/schedule/time?id=4",
+      calendarId:process.env.NEXT_PUBLIC_REACT_APP_CALENDAR_ID_4,
+      accessToken: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_ACCESS_TOKEN_4,
+      apiKey: process.env.NEXT_PUBLIC_REACT_APP_GOOGLE_API_KEY_4
     },
   ];
   
