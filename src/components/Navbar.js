0f8fb252833c2b7d4c4465cb27cfcf5b18192b49
aparg <apargtm@gmail.com>
2024-06-19 12:29:34 +0545

    style: navbar for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -97,7 +97,7 @@ const Navbar = ({}) => {
           </div>
         </div>
       </nav>
-      <div className="flex justify-between sm:hidden py-2">
+      <div className="flex justify-between sm:hidden py-2 mx-2">
         <div className="w-36">
           <Link href="/">
             <img src="/logo/logo1.webp" className="w-full"></img>
@@ -122,7 +122,11 @@ const Navbar = ({}) => {
               d="M4 8h16M4 16h16"
             />
           </svg> */}
-          <img src="/hamburger.svg" alt="toggle-nav" />
+          <img
+            src="/hamburger.svg"
+            alt="toggle-nav"
+            className="h-[24px] w-[24px]"
+          />
           <svg
             className="hidden w-6 h-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -140,88 +144,93 @@ const Navbar = ({}) => {
         </button>
       </div>
       {/* Mobile Navbar */}
-      <nav
-        className={`z-[999] py-4 bg-[#000] shadow-md w-[100vw] flex justify-center h-[100dvh] ${
-          hidden
-            ? "transform translate-x-[500%] duration-300 hidden"
-            : "transform translate-x-[0%] duration-300 sticky top-0"
-        } lg:hidden`}
-      >
-        <div className="flow-root w-full">
-          <div className="flex flex-col items-center px-6 my-2 w-full">
-            <div className="flex w-full justify-end h-10">
-              <img src="/cross.svg" onClick={() => setHidden(true)}></img>
+      <div className="h-[100vh] z-999 fixed top-0">
+        <nav
+          className={`z-[999] py-4 bg-[#000] shadow-md w-[100vw] flex justify-center h-[100dvh] ${
+            hidden
+              ? "transform translate-x-[500%] duration-300 hidden"
+              : "transform translate-x-[0%] duration-300 sticky top-0"
+          } lg:hidden`}
+        >
+          <div className="flow-root w-full">
+            <div className="flex flex-col items-center px-6 my-2 w-full">
+              <div className="flex w-full justify-end h-10">
+                <img
+                  src="/cross.svg"
+                  onClick={() => setHidden(true)}
+                  className="h-[24px] w-[24px]"
+                ></img>
+              </div>
+              <Link
+                href="/"
+                title=""
+                className={`text-base font-normal text-xl transition-all duration-200 py-2 text-white hover:text-primary-color mt-6`}
+                onClick={() => setHidden(true)}
+              >
+                {" "}
+                Home
+              </Link>
+              <Link
+                href={`/market-news`}
+                title=""
+                className={`text-base font-normal text-xl transition-all duration-200 py-2 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                {" "}
+                Market News
+              </Link>
+              <Link
+                href="/pre-construction/daniels-on-parliament"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Pre-constructions
+              </Link>
+              <Link
+                href="/rental"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Rental
+              </Link>
+              <Link
+                href="/assignments"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Assignments
+              </Link>
+              <Link
+                href="/resale"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Resale
+              </Link>
+              <Link
+                href="/contact"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Contact
+              </Link>
+              <Link
+                href="/schedule"
+                title=""
+                className={`inline-flex py-2 text-base font-normal text-xl transition-all duration-200 text-white hover:text-primary-color `}
+                onClick={() => setHidden(true)}
+              >
+                Schedule a Call
+              </Link>
             </div>
-            <div></div>
-            <Link
-              href="/"
-              title=""
-              className={`text-base font-normal transition-all text-lg duration-200 py-2 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              {" "}
-              Home
-            </Link>
-            <Link
-              href={`/market-news`}
-              title=""
-              className={`text-base font-normal transition-all text-lg duration-200 py-2 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              {" "}
-              Market News
-            </Link>
-            <Link
-              href="/pre-construction/daniels-on-parliament"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Pre-constructions
-            </Link>
-            <Link
-              href="/rental"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Rental
-            </Link>
-            <Link
-              href="/assignments"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Assignments
-            </Link>
-            <Link
-              href="/resale"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Resale
-            </Link>
-            <Link
-              href="/contact"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Contact
-            </Link>
-            <Link
-              href="/schedule"
-              title=""
-              className={`inline-flex py-2 text-base font-normal transition-all text-lg duration-200 text-white hover:text-primary-color `}
-              onClick={() => setHidden(true)}
-            >
-              Schedule a Call
-            </Link>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   );
 };
